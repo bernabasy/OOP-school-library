ef62dc3ce9cbce8518c8d4cdbd499da1f5cf13b0
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-13 20:23:40 +0300

    Fix linter error

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = 'Unknown', parent_permission : true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
