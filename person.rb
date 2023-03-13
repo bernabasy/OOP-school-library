db610121790884ecc0696312b193e193df6fa83f
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-13 19:05:22 +0300

    Fix linter error

--- person.rb
+++ person.rb
@@ -1,22 +1,22 @@
 class Person
-def initialize(age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = "Unknown", parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    end
-attr_reader :id
+  end
+  attr_reader :id
 
- attr_accessor :name, :age
+  attr_accessor :name, :age
 
- 
- def can_use_services?
+
+  def can_use_services?
     of_age? || @parent_permission
   end
 
   private
 
   def is_of_age?
-    @age >=18 
+    @age >= 18
   end
-end       
+end
