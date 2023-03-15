3d754b01bc81373fe5dd2408b5cc9d183458ed46
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-15 13:31:18 +0300

    Create student belongs to a classroom relationship

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
