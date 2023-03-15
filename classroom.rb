466600cc0a4fce40f130dd4cf901f0293b8668e6
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-15 13:29:12 +0300

    Create a class Classroom

---
+++ classroom.rb
@@ -0,0 +1,12 @@
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_students(student)
+    @students.push.student
+  end
+end
