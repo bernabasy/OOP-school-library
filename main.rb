ef860e38a401b28b849a3138e440eb15957f4855
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-17 14:24:46 +0300

    list of options for book, rentals parameter

--- main.rb
+++ main.rb
@@ -30,3 +30,68 @@ def teacher_student(option)
     puts 'Person created successfully'
   end
 end
+
+def option_create_book
+  print 'Title: '
+  title = gets.chomp
+  print 'Author: '
+  author = gets.chomp
+  create_book(title, author)
+  puts 'Book created successfully'
+end
+
+def option_create_person
+  print 'Do you want to createa student (1) or a teacher (2)? [Input the number]: '
+  create = gets.chomp
+  teacher_student(create)
+end
+
+def option_create_rental
+  puts 'Select a book from the following list by number'
+  state = list_rent_books
+  if state == 'empty'
+    puts 'Books shelf is empty.'
+    return
+  end
+  book_no = gets.chomp
+  puts 'Select a person from the folowing list by number (not id)'
+  person_present = list_rent_people
+  if person_present == 'empty'
+    puts 'There are no people to list.'
+    return
+  end
+  person_no = gets.chomp
+  print 'Date: '
+  date = gets.chomp
+  create_rental(person_no, book_no, date)
+  puts 'Rental created sucessfully'
+end
+
+def option_list_rentals
+  print 'ID of person: '
+  person_id = gets.chomp
+  list_rental(person_id)
+end
+
+def exit
+  puts 'Thank you for using this app.'
+end
+
+def invalid_option
+  puts 'Kindly enter a valid option'
+end
+
+def parameter_option(parameter)
+  options = {
+    '1' => method(:list_books),
+    '2' => method(:list_people),
+    '3' => method(:option_create_person),
+    '4' => method(:option_create_book),
+    '5' => method(:option_create_rental),
+    '6' => method(:option_list_rentals),
+    '7' => method(:exit)
+  }
+
+  option_method = options[parameter] || method(:invalid_option)
+  option_method.call
+end
