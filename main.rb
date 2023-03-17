8aff4ddf651b9c71760014ec9686df9d9523415d
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-17 14:21:42 +0300

    Create a list of options for teacher and student

--- main.rb
+++ main.rb
@@ -7,3 +7,26 @@ def list_options
   puts
   options.each { |option| puts option }
 end
+
+def teacher_student(option)
+  case option
+  when '1'
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp
+    create_person(1, age, name, parent_permission)
+    puts 'Person created successfully'
+  when '2'
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    create_person(2, age, name, specialization)
+    puts 'Person created successfully'
+  end
+end
