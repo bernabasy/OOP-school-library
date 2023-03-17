bd12365f70b3946c9820c4c9d5927136abdcbba4
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-17 14:25:53 +0300

    Fix eslint

--- rental.rb
+++ rental.rb
@@ -1,13 +1,13 @@
 class Rental
   attr_accessor :date, :person, :book
 
-  def initialize(date, person, book)
-    @date = date
-
+  def initialize(person, book, date)
     @person = person
     person.rentals << self
 
     @book = book
     book.rentals << self
+
+    @date = date
   end
 end
