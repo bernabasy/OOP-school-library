d3e7aceb9cf9f33683e6ddf2e93def12000a50f3
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-21 20:12:22 +0300

    Refactor main to SOLID

--- main.rb
+++ main.rb
@@ -1,27 +1,4 @@
-require './app'
-
-def list_options
-  options = ['Please choose an option by entering a number', '1 - List all books', '2 - List all people',
-             '3 - Create a person', '4 - Create a book', '5 - Create a rental',
-             '6 - List all rentals for a given person id', '7 - Exit']
-  puts
-  options.each { |option| puts option }
-end
-
-def parameter_option(parameter)
-  options = {
-    '1' => method(:list_books),
-    '2' => method(:list_people),
-    '3' => method(:option_create_person),
-    '4' => method(:option_create_book),
-    '5' => method(:option_create_rental),
-    '6' => method(:option_list_rentals),
-    '7' => method(:exit)
-  }
-
-  option_method = options[parameter] || method(:invalid_option)
-  option_method.call
-end
+require './options'
 
 def main
   app = App.new
